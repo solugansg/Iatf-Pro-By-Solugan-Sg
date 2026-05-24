5d40306cdcfe7eb44170e959af79e68c9fc893f8
Solugan Sg <solugansg@gmail.com>
2026-05-24 01:34:57 -0500

    chore: bump version to v2.8.12 and update service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Iatf Pro by Solugan SG - Service Worker v2.8.11
-const CACHE_NAME = 'iatfpro-v2.8.11';
+// Iatf Pro by Solugan SG - Service Worker v2.8.12
+const CACHE_NAME = 'iatfpro-v2.8.12';
 
 // Todos los archivos que se guardan en caché para uso offline
 const ASSETS_TO_CACHE = [
