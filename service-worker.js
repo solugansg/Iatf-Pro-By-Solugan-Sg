a6a1abb4bb774cd40334af912866fed41ff656dc
Solugan Sg <solugansg@gmail.com>
2026-05-26 09:40:47 -0500

    fix: caching issues on vercel preventing users from seeing new versions

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Iatf Pro by Solugan SG - Service Worker v2.8.42
-const CACHE_NAME = 'iatfpro-v2.8.42';
+// Iatf Pro by Solugan SG - Service Worker v2.8.43
+const CACHE_NAME = 'iatfpro-v2.8.43';
 
 // Todos los archivos que se guardan en caché para uso offline
 const ASSETS_TO_CACHE = [
