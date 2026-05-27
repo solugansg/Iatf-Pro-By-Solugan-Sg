0d47103626a35358d7a31a6b09f35ab9077d4bac
Solugan Sg <solugansg@gmail.com>
2026-05-26 19:26:59 -0500

    Ajustar layout del encabezado reduciendo tamano de Finca a la mitad e incrementando Ubicacion al doble. Bump v2.8.62.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Iatf Pro by Solugan SG - Service Worker v2.8.61
-const CACHE_NAME = 'iatfpro-v2.8.61';
+// Iatf Pro by Solugan SG - Service Worker v2.8.62
+const CACHE_NAME = 'iatfpro-v2.8.62';
 
 // Todos los archivos que se guardan en caché para uso offline
 const ASSETS_TO_CACHE = [
