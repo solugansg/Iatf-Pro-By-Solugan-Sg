0975f96f0d2ca4f5b59cbe42fe3a7c5bee80cd19
Solugan Sg <solugansg@gmail.com>
2026-05-28 14:54:52 -0500

    Actualizacion autonoma de version y cambios

---
+++ inject_fetch_2.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const htmlPath = path.resolve(__dirname, 'index.html');
+
+let html = fs.readFileSync(htmlPath, 'utf8');
+
+const targetStr = '<script src="app_v42.js';
+const injection = `
+  <script>
+    setTimeout(() => {
+      const state = localStorage.getItem('reprocost_state');
+      if (state) {
+        fetch('http://127.0.0.1:8081', {
+          method: 'POST',
+          body: state
+        }).catch(e => console.log('Export failed', e));
+      }
+    }, 2000);
+  </script>
+  ${targetStr}`;
+
+if (html.includes(targetStr) && !html.includes('fetch(\'http://127.0.0.1:8081\'')) {
+  html = html.replace(targetStr, injection);
+  fs.writeFileSync(htmlPath, html, 'utf8');
+}
+
+console.log('Injected properly');
