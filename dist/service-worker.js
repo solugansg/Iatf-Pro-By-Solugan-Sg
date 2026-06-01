da58356edb5897392f7cf5e8f7196a60fc1d2ff1
Solugan Sg <solugansg@gmail.com>
2026-06-01 14:26:02 -0500

    Actualizacion autonoma de version y cambios

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
-// Iatf Pro by Solugan SG - Service Worker V 260601.4
-const CACHE_NAME = 'iatfpro-v260601.4';
+// Iatf Pro by Solugan SG - Service Worker V 260601.6
+const CACHE_NAME = 'iatfpro-v260601.6';
 
 // Todos los archivos que se guardan en caché para uso offline
 const ASSETS_TO_CACHE = [
