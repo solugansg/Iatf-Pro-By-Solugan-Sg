8bd0cc683a87b69484a193c846d9d72756f7fcef
Solugan Sg <solugansg@gmail.com>
2026-06-03 13:37:26 -0500

    Implement mobile visual improvements (Bottom nav, cards, touch targets)

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
-// Iatf Pro by Solugan SG - Service Worker V 260603.11
-const CACHE_NAME = 'iatfpro-v260603.11';
+// Iatf Pro by Solugan SG - Service Worker V 260603.12
+const CACHE_NAME = 'iatfpro-v260603.12';
 
 // Todos los archivos que se guardan en caché para uso offline
 const ASSETS_TO_CACHE = [
