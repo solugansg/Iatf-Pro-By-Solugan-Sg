4917a27a84503ca374843fa186e4801f2498b545
Solugan Sg <solugansg@gmail.com>
2026-06-03 13:17:29 -0500

    Bump version to push update for matrix language re-rendering

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
-// Iatf Pro by Solugan SG - Service Worker V 260603.8
-const CACHE_NAME = 'iatfpro-v260603.8';
+// Iatf Pro by Solugan SG - Service Worker V 260603.9
+const CACHE_NAME = 'iatfpro-v260603.9';
 
 // Todos los archivos que se guardan en caché para uso offline
 const ASSETS_TO_CACHE = [
