44b1cf3734141a7cc5235e14308de8aefa45ae29
Solugan Sg <solugansg@gmail.com>
2026-06-03 16:02:13 -0500

    Remove green mobile sidebar handle

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
-// Iatf Pro by Solugan SG - Service Worker V 260603.30
-const CACHE_NAME = 'iatfpro-v260603.30';
+// Iatf Pro by Solugan SG - Service Worker V 260603.31
+const CACHE_NAME = 'iatfpro-v260603.31';
 
 // Todos los archivos que se guardan en caché para uso offline
 const ASSETS_TO_CACHE = [
