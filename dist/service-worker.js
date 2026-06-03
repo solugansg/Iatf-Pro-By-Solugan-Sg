8919edb2905c332aa9768a9f1e34855fe1596ac6
Solugan Sg <solugansg@gmail.com>
2026-06-03 13:27:54 -0500

    Fix Receptoras (Te) mapping for translations

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
-// Iatf Pro by Solugan SG - Service Worker V 260603.10
-const CACHE_NAME = 'iatfpro-v260603.10';
+// Iatf Pro by Solugan SG - Service Worker V 260603.11
+const CACHE_NAME = 'iatfpro-v260603.11';
 
 // Todos los archivos que se guardan en caché para uso offline
 const ASSETS_TO_CACHE = [
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Iatf Pro by Solugan SG - Service Worker V 260530.2
-const CACHE_NAME = 'iatfpro-V260603.10';
+const CACHE_NAME = 'iatfpro-V260603.11';
 
 // Todos los archivos que se guardan en caché para uso offline
 const ASSETS_TO_CACHE = [
